9b5fa364a109de1b2198d0b5f66af7d95a626188
Neal Noble <nrnoble@hotmail.com>
2017-05-05 19:02:51 -0700

    Assignment 99% complete

---
+++ it333/Iterative Binary Search Tree/src/BinarySearchTree.java
@@ -0,0 +1,396 @@
+import sun.plugin2.applet.context.NoopExecutionContext;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Stack;
+
+// Done - The addUpdate() method adds a new element to your tree or updates an existing element.
+// Done - The size(), isEmpty() and clear() methods correctly track or remove elements in the BST.
+// Done - The inOrder() method returns an ordered list of unique elements from the BST.
+// Done - Your informal tests on the BST test each of the listed scenarios.
+
+
+public class BinarySearchTree<T extends Comparable<T>>
+{
+    private Node root;
+    private int size;
+
+    public BinarySearchTree()
+    {
+
+    }
+
+    /**
+     * Adds or updates an element in the binary search tree.
+     * @param element to be added or updated.
+     * @return  true when a new element is added and false if an update occurred.
+     */
+    public boolean addUpdate(T element)
+    {
+        Node node = new Node(element);
+
+        if (this.root == null)
+        {
+            this.root = node;
+            size++;
+            return true;
+        }
+
+        Node currentNode = this.root;
+
+        while (true)
+        {
+            int compare = currentNode.data.compareTo(element);
+            if (compare > 0)
+            {
+                if (currentNode.left != null)
+                    currentNode = currentNode.left;
+                else
+                {
+                    currentNode.left = node;
+                    size++;
+                    break;
+                }
+            }
+            else if (compare < 0)
+            {
+                if (currentNode.right != null)
+                    currentNode = currentNode.right;
+                else
+                {
+                    currentNode.right = node;
+                    size++;
+                    break;
+                }
+            }
+            else
+            {
+                currentNode.data = node.data;
+                size++;
+                break;
+            }
+        }
+
+        return true;
+    }
+
+
+    /**
+     *  Searches Binary tree for an element.
+     * @param element that is to be searched for in tree
+     * @return
+     */
+    public boolean contains(T element)
+    {
+        Node currentNode;
+        currentNode = this.root;
+
+        while (currentNode != null)
+        {
+            int compare = currentNode.data.compareTo(element);
+            if (compare > 0)
+            {
+                if (currentNode.left != element)
+                    currentNode = currentNode.left;
+            }
+            else if (compare < 0)
+            {
+                if (currentNode.right != element)
+                    currentNode = currentNode.right;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    /**
+     * Gets the value associated with the input element key. If no key is found this method will return null.
+     * @param element key pair
+     * @return the value associated with the input key. If no key is found this method will return null.
+     */
+    public T get(T element)
+    {
+        Node currentNode;
+        currentNode = this.root;
+
+        while (currentNode != null)
+        {
+            int compare = currentNode.data.compareTo(element);
+            if (compare > 0)
+            {
+                if (currentNode.left != element)
+                    currentNode = currentNode.left;
+            }
+            else if (compare < 0)
+            {
+                if (currentNode.right != element)
+                    currentNode = currentNode.right;
+            }
+            else
+            {
+                return currentNode.data;
+            }
+        }
+        return null;
+    }
+
+
+
+    /**
+     * 	The number of elements in the tree.
+     * @return 	the number of elements in the tree.
+     */
+    public int size()
+    {
+        return size;
+    }
+
+
+    /**
+     * This returns true if there are no elements in the tree, but otherwise returns false.
+     * @return true if there are not elements, otherwise false.
+     */
+    public boolean isEmpty()
+    {
+        return size == 0;
+    }
+
+    /**
+     * This removes all elements in the tree. Sets size to zero
+     */
+    public void clear()
+    {
+        root = null;
+        size = 0;
+    }
+
+
+
+    /**
+     * Returns a sorted list of all elements in the tree, using the In-Order tree traversal of the underlying structure.
+     * @return a sorted list of all elements
+     */
+    public  List<T> postOrder()
+    {
+        List<T> elementList = new ArrayList<T>();
+       return postOrder(this.root, elementList);
+    }
+
+    private  List<T> postOrder(Node currentNode, List<T> list)
+    {
+
+        Stack<Node> stackOfNodes = new Stack<>();
+
+        while (true)
+        {
+            while (currentNode != null)
+            {
+                stackOfNodes.push(currentNode);
+                currentNode = currentNode.right;
+            }
+
+            if (stackOfNodes.isEmpty())
+            {
+                return list;
+            }
+            
+            currentNode = stackOfNodes.pop();
+            list.add(currentNode.data);
+            currentNode = currentNode.left;
+        }
+
+    }
+
+
+    /**
+     * return a list of items in order
+     * @return order list of items
+     */
+    public  List<T> inOrder()
+    {
+        List<T> elementList = new ArrayList<T>();
+        return inOrder(this.root, elementList);
+    }
+
+    private  List<T> inOrder(Node currentNode, List<T> list)
+    {
+        Stack<Node> stackOfNodes = new Stack<>();
+        //stackOfNodes.push(currentNode);
+
+        while (true)
+        {
+            while (currentNode != null)
+            {
+                stackOfNodes.push(currentNode);
+                currentNode = currentNode.left;
+
+            }
+
+            if (stackOfNodes.isEmpty())
+            {
+                return list;
+            }
+
+            currentNode = stackOfNodes.pop();
+            list.add(currentNode.data);
+            currentNode = currentNode.right;
+        }
+
+
+
+
+//        if (currentNode != null)
+//        {
+//            inOrder(currentNode.left,list);
+//            list.add(currentNode.data);
+//            //System.out.println(currentNode.data);
+//            inOrder(currentNode.right,list);
+//        }
+        //return list;
+    }
+
+
+
+
+    public  List<T> preOrder()
+    {
+        List<T> elementList = new ArrayList<T>();
+        return preOrder(this.root, elementList);
+    }
+
+    private  List<T> preOrder(Node currentNode, List<T> list)
+    {
+//        if (current != null)
+//        {
+//            System.out.println(current.data);
+//            preOrder(current.left);
+//            preOrder(current.right);
+//        }
+
+        Stack<Node> stackOfNodes = new Stack<>();
+
+//        while (true)
+//        {
+////            while (currentNode != null)
+////            {
+////                stackOfNodes.push(currentNode);
+////                list.add(currentNode.data);
+////                currentNode = currentNode.left;
+////                currentNode = currentNode.right;
+////            }
+//
+////            if (stackOfNodes.isEmpty())
+////            {
+////                return list;
+////            }
+//        }
+
+
+        stackOfNodes.push(currentNode);
+
+
+        while (stackOfNodes.empty() == false)
+        {
+
+
+            currentNode = stackOfNodes.peek();
+            list.add(currentNode.data);
+
+            stackOfNodes.pop();
+
+            if (currentNode.right != null)
+            {
+                stackOfNodes.push(currentNode.right);
+            }
+
+            if (currentNode.left != null)
+            {
+                stackOfNodes.push(currentNode.left);
+            }
+        }
+
+            return list;
+
+
+    }
+
+
+
+
+
+    public Iterator<T> iterator()
+    {
+        return new BSTIterator(root);
+    }
+
+
+    private class BSTIterator implements Iterator<T>
+    {
+        private Stack<Node> nodeStack = new Stack<>();
+
+        public BSTIterator (Node current)
+        {
+            while (current != null)
+            {
+                nodeStack.push(current);
+                current= current.left;
+            }
+        }
+
+        @Override
+        public boolean hasNext()
+        {
+            return !nodeStack.isEmpty();
+        }
+
+
+        @Override
+        public T next()
+        {
+            Node next = nodeStack.pop();
+
+            if(next.right!= null)
+            {
+                nodeStack.push(next.right);
+                Node current = next.right;
+                while (current.left != null)
+                {
+                    nodeStack.push(current.left);
+                    current = current.left;
+                }
+            }
+
+            return next.data;
+        }
+    }
+
+
+
+
+
+    private class Node
+    {
+        private T data;
+        private Node left;
+        private Node right;
+
+        public Node (T data)
+        {
+            this.data=data;
+        }
+
+        public String toString()
+        {
+            String dataString = (data == null) ? "null": data.toString();
+            String leftChild = (left == null) ? "null": left.data.toString();
+            String rightChild = (right == null) ? "null": right.data.toString();
+
+            return leftChild + " <--" + dataString + " --> " + rightChild;
+        }
+    }
+}
---
+++ it333/Iterative Binary Search Tree/src/Node.java
@@ -0,0 +1,93 @@
+//
+//
+//private class Node
+//{
+//    private T data;
+//    private Node left;
+//    private Node right;
+//
+//    public Node (T data)
+//    {
+//        this.data=data;
+//    }
+//
+//    public String toString()
+//    {
+//        String dataString = (data == null) ? "null": data.toString();
+//        String leftChild = (left == null) ? "null": left.data.toString();
+//        String rightChild = (right == null) ? "null": right.data.toString();
+//
+//        return leftChild + " <--" + dataString + " --> " + rightChild;
+//    }
+//}
+
+
+
+//public class T Node
+//    private T element;
+//    private Node left, right;
+//
+//    public Node(T element)
+//    {
+//        this.element = element;
+//        this.left = null;
+//        this.right = null;
+//    }
+//
+//    /**
+//     *  Get node element
+//     * @return node element
+//     */
+//    public int getElement()
+//    {
+//        return element;
+//    }
+//
+//    /**
+//     * Set node Element
+//     * @param element value
+//     */
+//    public void setElement(int element)
+//    {
+//        this.element = element;
+//    }
+//
+//    /**
+//     * Get left Node element
+//     * @return left Node element
+//     */
+//    public Node getLeft()
+//    {
+//        return left;
+//    }
+//
+//    /**
+//     * Set left Node element
+//     * @param left node element
+//     */
+//    public void setLeft(Node left)
+//    {
+//        this.left = left;
+//    }
+//
+//    /**
+//     * get Right node element
+//     * @return right node
+//     */
+//    public Node getRight()
+//    {
+//        return right;
+//    }
+//
+//    /**
+//     * Set right node element
+//     * @param right node elment
+//     */
+//    public void setRight(Node right)
+//    {
+//        this.right = right;
+//    }
+
+
+
+//}
