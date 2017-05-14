c38db16e29a74540fed5193fe0715d2c5149f5ea
Neal Noble <nrnoble@hotmail.com>
2017-05-14 04:31:22 -0700

    Adding comments

--- it328/328/Intro_JS/intro.js
+++ it328/328/Intro_JS/intro.js
@@ -6,6 +6,12 @@
  * Instructor: Tina Ostrander
  */
 
+// Create a file called intro.js in your 328 directory.
+// Add a statement to your file that prints "Introduction
+// to JavaScript" to the console window, and test it in
+// your browser.
+
+// This function is tested in "test_script.php"
 function intro()
 {
     var comment = "Introduction to JavaScript";
