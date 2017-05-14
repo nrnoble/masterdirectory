f9af5922a59a847fcdcec5afb0f5b46d24bcda15
Neal Noble <nrnoble@hotmail.com>
2017-05-14 01:09:30 -0700

    created project directory. Move assignment contents to project directory

---
+++ it328/328/Intro_JS/test_script.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <script src="intro.js"></script>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<script>
+    intro();
+</script>
+</body>
+</html>
