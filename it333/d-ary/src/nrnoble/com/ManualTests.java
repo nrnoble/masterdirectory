6ee84c84118487bf745e6b8680d55e7dc25f292c
Neal Noble <nrnoble@hotmail.com>
2017-06-02 01:12:09 -0700

    final update

--- it333/d-ary/src/nrnoble/com/ManualTests.java
+++ it333/d-ary/src/nrnoble/com/ManualTests.java
@@ -12,32 +12,11 @@ public class ManualTests
         MaryHeap dh = new MaryHeap(10,3);
        // MarryHeap<Integer> dh = new MarryHeap<>();
 
-        // int[] input = {140,130,120,110,100,90,80,70,60,5,40,30,20,10};
-        // int[] input = {14,13,12,11,10,9,8,7,6,5,4,3,2,1};
-         int[] input = {9,8,7,6,5,4,3,2,1};
-   //      int[] input = {1,2,3,4,5,6,7,8,9,10,11,12};
-     //    int[] input = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
-      //   String [] input = {"a", "c", "b", "a"};
-        // int[] input = {6, 22, 5, 77, 12, 11, 42, 150, 11, 11, 11};
-       //  int[] input = {22,12,13,25,18,20,6,9,2,24,17,5,4,0,3,21,11,1,10,19,14,23,8,7,16,15};         // 000 001 003 002 004 006 007 008 009 010 011 013 012 014 005 015 016 017 018 019 020 021 022 023 024 025
+        //   int[] input = {9,8,7,6,5,4,3,2,1};
+        //   int[] input = {1,2,3,4,5,6,7,8,9,10,11,12};
+        //   String [] input = {"a", "c", "b", "a"};
 
-
-        // int[] input = {12,11,10,9,8,7,6,5,4,3,2,1};
-       // int[] input = {13,15,8,7,14,00,6,9};
-       // int[] input = {8,9,0,2,13,11,7};
-      //  int[] input = {5,3,1,0,4,2};
-       // int[] input = {2,1,4,3,11,17,0};
-        // int[] input = getRandomNumbers(25);
-       //  int[] input = {25,8,32,9,5,6,14,4,11,12,3, 2, 1};
-
-            // Broken [1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 6, 12, 13, 15, 16, 17, 18, 19, 14, 20, 21, 22, 23, 24, 25]
-
-        // Working MaryHeap Order:           [0]:01, [1]:5, [2]:02, [3]:14, [4]:12, [5]:09, [6]:6, [7]:03, [8]:19, [09]:18, [10]:23, [11]:16, [12]:15, [13]:25, [14]:20, [15]:13, [16]:22, [17]:11, [18]:10, [19]:21, [20]:08, [21]:07, [22]:24, [23]:17, [24]:04
-                    // not working [0]:null, [1]:01, [2]:5, [3]:02, [4]:14, [5]:12, [6]:09, [7]:6, [8]:03, [9]:19, [10]:18, [11]:23, [12]:16, [13]:15, [14]:25, [15]:20, [16]:13, [17]:22, [18]:11, [19]:10, [20]:21, [21]:08, [22]:07, [23]:24, [24]:17
-    //       int[] input = {25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1};
-           //String[] input = {"z","y","x","w","v","u","t","s","r","q","p","o","n","m", "l", "k", "j", "i", "h", "g", "f", "e", "d", "c", "b", "a"};
-
-        dh.printHeap();
+         int[] input = getRandomNumbers(12);
 
         for (int i = 0; i < input.length; i++)
         {
@@ -48,26 +27,11 @@ public class ManualTests
 
         System.out.println("Are element in the correct order: " + dh.verifyHeapOrder());
 
-        MaryHeap dh2 = maryClassDump(dh);
+
         System.out.println();
         System.out.println();
 
-
-
-
-
-//        System.out.println();
-//        System.out.println();
-//        System.out.println();
-//
-//        MaryHeap dh3 = maryClassDump(dh2);
-//        System.out.println();
-//        System.out.println();
-//        MaryHeap dh4 = maryClassDump(dh3);
-//        System.out.println();
-//        System.out.println();
-//        MaryHeap dh5 = maryClassDump(dh4);
-
+        maryClassDump(dh);
 
     }
 
@@ -89,13 +53,9 @@ public class ManualTests
     {
         for (int i = 0; i < dh2.size()-1 ; i++)
         {
-
             System.out.println(" dh2.getIndexOfSmallestChild(i): " + String.format("%03d", dh2.getIndexOfSmallestChild(i))  +" (" + String.format("%03d", dh2.data[dh2.getIndexOfSmallestChild(i)]  )+ ") ");
             System.out.println("dh2.getIndexOfSmallestChild2(i): " + String.format("%03d", dh2.getIndexOfSmallestChild2(i)) +" (" + String.format("%03d", dh2.data[dh2.getIndexOfSmallestChild2(i)]  ) + ") ");
             System.out.println();
-
-            //System.out.println(dh2.getIndexOfSmallestChild(i));
-            //System.out.println(dh2.getIndexOfSmallestChild2(i));
         }
     }
 
--- it333/d-ary/src/nrnoble/com/UnitTests.java
+++ it333/d-ary/src/nrnoble/com/UnitTests.java
@@ -245,7 +245,9 @@ public class UnitTests<T extends Comparable<T>>
         String name = new Object(){}.getClass().getEnclosingMethod().getName();
         System.out.println("running test: " + name);
         data.clear();
-        fillData(10000);
+        Object[] randnums = getRandomNumbers(10000);
+        this.fillData(randnums);
+       // fillData(10000);
         if (data.size() != 10000)
         {
             Assert.assertTrue(name + " test has failed", false);
@@ -256,11 +258,12 @@ public class UnitTests<T extends Comparable<T>>
 
     // Insert 1 million elements and verify size
     @Test
-    public void insert1MillElements() throws Exception
+    public void MillionElements() throws Exception
     {
         String name = new Object(){}.getClass().getEnclosingMethod().getName();
         System.out.println("running test: " + name);
         data.clear();
+
         fillData(1000000);
         if (data.size() != 1000000)
         {
@@ -301,33 +304,9 @@ public class UnitTests<T extends Comparable<T>>
         System.out.println("running test: " + name);
 
         data.clear();
-       // insert10KElements();
-
-        insert1MillElements();
-
-        // verifty that all elements are sorted properly
-        T child = (T) data.delMin();
-        for (int i = 0; i < data.size() ; i++)
-        {
-            T parent = (T) data.delMin();
-
-
-
-            if (parent.compareTo(child) < 0)
-            {
-              //  System.out.println("parent: " + parent + " < " + child + " : child   fail");
-                Assert.assertTrue(name + " test has failed", false);
-                return;
-            }
-          //  System.out.println("parent: " + parent + " > " + child + " : child   pass ");
-
-            child = parent;
-        }
-
-        System.out.println();
+        MillionElements();
+        data.verifyHeapOrder();
     }
-
-
 
 
     /// Private Test Helpers ///
